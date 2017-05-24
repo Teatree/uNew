a3cfbae978058115b99671c0199d49afc19751e8
Teatree <1322r88no>
2017-05-24 18:04:45 +0300

    * start

---
+++ Assets/Model/Tile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile {
+
+	public enum Type {Empty, Floor };
+
+	Type type = Type.Empty;
+
+	LooseObj looseObj;
+	InstalledObj installedObj;
+
+	World world;
+	int x;
+	int y;
+
+	public Tile(World world, int x, int y){
+		this.world = world;
+		this.x = x;
+		this.y = y;
+	}
+}
